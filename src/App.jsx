6aa211413f0e5533a269349e3354205e0305cb71
Soumyadip02 <soumyadipbhattacharya02@gmail.com>
2026-07-16 23:32:57 +0530

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useEffect } from "react";
 import Login from "./components/login";
 import Register from "./components/register";
 import Dashboard from "./components/dashboard";
@@ -6,6 +7,26 @@ import ProtectedRoute from "./components/auth/ProtectedRoute";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
 
 function App() {
+  useEffect(() => {
+    const existingScript = document.getElementById("gtag-js");
+    if (!existingScript) {
+      const script = document.createElement("script");
+      script.id = "gtag-js";
+      script.async = true;
+      script.src = "https://www.googletagmanager.com/gtag/js?id=G-KRDZCX48BB";
+      document.head.appendChild(script);
+      script.onload = () => {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(...args) {
+          window.dataLayer.push(args);
+        }
+        window.gtag = window.gtag || gtag;
+        window.gtag("js", new Date());
+        window.gtag("config", "G-KRDZCX48BB");
+      };
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <AuthProvider>
